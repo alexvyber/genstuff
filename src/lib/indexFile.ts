ef12845b7305c277584462d25ba30edc624e5588
Alexey Sokolov <alexvyber@gmail.com>
2023-03-05 16:17:58 +0300

    a15

--- src/lib/indexFile.ts
+++
@@ -1,6 +0,0 @@
-class IndexFile {
-  public static getIndex(componentName: string, defaultExport: boolean = false) {
-    if (defaultExport) return `export default from "./${componentName}"`
-    return `export {${componentName}} from "./${componentName}"`
-  }
-}

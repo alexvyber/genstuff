f53c68e960b7ea0980f17b01899ddc5961ef0818
Alexey Sokolov <alexvyber@gmail.com>
2024-06-17 22:35:52 +0300

    next layout

---
+++ test/commands/next/layout.test.ts
@@ -0,0 +1,14 @@
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+
+describe('next:layout', () => {
+  it('runs next:layout cmd', async () => {
+    const {stdout} = await runCommand('next:layout')
+    expect(stdout).to.contain('hello world')
+  })
+
+  it('runs next:layout --name oclif', async () => {
+    const {stdout} = await runCommand('next:layout --name oclif')
+    expect(stdout).to.contain('hello oclif')
+  })
+})

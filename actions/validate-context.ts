734306f367a64c1e68dbf64cc15a8e6c4d852a36
Alexey Sokolov <alexey@brand-map.online>
2025-09-17 11:43:05 +0300

    drop

--- actions/validate-context.ts
+++
@@ -1,5 +0,0 @@
-import type { RunGeneratorActionFn } from "../core/runner.ts"
-
-export function validateContext(): RunGeneratorActionFn {
-  return async function execute(_params): Promise<void> {}
-}

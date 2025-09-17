ede25e7d58627b70f2ce7819dacddc56ad53b1f9
Alexey Sokolov <alexey@brand-map.online>
2025-09-17 17:32:23 +0300

    0.0.5

--- mod.ts
+++ mod.ts
@@ -1,16 +1,5 @@
 // Run
 export { run } from "./runner.ts"
 
-// Actions
-export { ai } from "./actions/ai.ts"
-export { answer } from "./actions/answer.ts"
-export { config } from "./actions/config.ts"
-export { modify } from "./actions/modify.ts"
-export { use } from "./actions/use.ts"
-export { prompt } from "./actions/prompt.ts"
-export { write } from "./actions/write.ts"
-export { validateAnswers } from "./actions/validate_answers.ts"
-export { loadTemplates } from "./actions/load_templates.ts"
-
 // Helpers
 export { textHelpers } from "./lib.ts"
